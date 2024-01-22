11400e489f470c170f363778a19d76c57e854530
clementiiineee <clementine.violette@ird.fr>
2024-01-22 16:31:45 +0400

    xtoken and get ref json

--- website/palangre_syc/views.py
+++ website/palangre_syc/views.py
@@ -3,10 +3,84 @@ import requests
 
 from django.http import HttpResponse
 
+
+
+def get_token():
+    '''
+    Fonction qui sort un Token 
+    Amélioration : on passe en entrée les données, obtenues à partir du formulaire de connexion 
+    '''
+    api_base = 'https://observe.ob7.ird.fr/observeweb/api/public/init/open?'
+    api_modelVersion = 'config.modelVersion=9.2.1&'
+    api_login = 'config.login=technicienweb&'
+    api_password = 'config.password=wpF3NITE&'
+    api_databaseName = 'config.databaseName=test&'
+    api_referential = 'referentialLocale=FR'
+    
+    # Constitution du lien url pour accéder à l'API et donc générer un token
+    api_url = api_base + api_modelVersion + api_login + api_password + api_databaseName + api_referential
+    response = requests.get(api_url)
+    
+    # si la réponse est un succès, on extrait que le Token
+    if response.status_code == 200:
+        data_from_api = response.json()
+        token = data_from_api['authenticationToken']
+    else:
+        token = None
+
+    return token
+
+def get_referential_ll():
+    '''
+    Fonction qui affiche le référentiel longliners 
+    '''
+    
+    api_base = 'https://observe.ob7.ird.fr/observeweb/api/public/referential/ll?'
+    api_Token = 'authenticationToken=' + get_token() +'&'
+    api_infos = 'config.loadReferential=&config.recursive=&config.prettyPrint=true&config.serializeNulls=&referentialLocale='
+    
+    # Constitution du lien url pour accéder à l'API et donc générer un token
+    api_url = api_base + api_Token + api_infos 
+    response = requests.get(api_url)
+    
+    # si la réponse est un succès, on extrait que le Token
+    if response.status_code == 200:
+        data_ref_ll = response.json()
+    else:
+        data_ref_ll = None
+
+    return data_ref_ll
+    
+def get_referential_common():
+    '''
+    Fonction qui affiche le référentiel common 
+    '''
+        
+    api_base = 'https://observe.ob7.ird.fr/observeweb/api/public/referential/common?'
+    api_Token = 'authenticationToken=' + get_token() +'&'
+    api_infos = 'config.loadReferential=&config.recursive=&config.prettyPrint=true&config.serializeNulls=&referentialLocale='
+        
+    # Constitution du lien url pour accéder à l'API et donc générer un token    
+    api_url = api_base + api_Token + api_infos 
+    response = requests.get(api_url)
+        
+    # si la réponse est un succès, on extrait que le Token
+    if response.status_code == 200:
+        data_ref_common = response.json()
+    else:
+        data_ref_common = None
+
+    return data_ref_common
+    
+    
 def index(request):
-    
-    
-    return render(request, "LL_homepage.html")
-
-
+    token = get_token()
+    data_ref_ll = get_referential_ll()
+    data_ref_common = get_referential_common()
+    context = {
+        'token': token, 
+        'data_ref_ll': data_ref_ll, 
+        'data_ref_common' : data_ref_common
+    }
+    return render(request, 'LL_homepage.html', context)
 
