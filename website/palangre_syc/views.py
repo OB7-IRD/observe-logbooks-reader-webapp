79b47abd647a535ad8454f4554f2d2452c0bfbaa
clementiiineee <clementine.violette@ird.fr>
2024-01-22 14:01:31 +0400

    Update views.py

--- website/palangre_syc/views.py
+++ website/palangre_syc/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+import requests
 
 from django.http import HttpResponse
-from django.shortcuts import render
-
 
 def index(request):
-    #return HttpResponse("Hello, Cette page est la home page de la partie palangre")
+    
+    
     return render(request, "LL_homepage.html")
+
+
+
